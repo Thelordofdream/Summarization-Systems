3de2420d4c2c4937e677f2d3950b2fdae4d18086
Thelordofdream <574483817@qq.com>
2017-10-23 12:20:15 +0800

    embedding

--- preporcess.py
+++ preporcess.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import gensim
 
 
 def cleanText(corpus):
@@ -14,6 +15,25 @@ def cleanText(corpus):
         corpus = [z.replace(c, ' ') for z in corpus]
     corpus = [z.split() for z in corpus]
     return corpus
+
+
+def buildWordVector(model_w2v, text, size):
+    sen = []
+    vec = np.zeros(size).reshape((1, size))
+    for word in text:
+        try:
+            vec = model_w2v[word].reshape((1, size))
+            sen.extend(vec)
+        except:
+            continue
+    return sen
+
+
+def storeVecs(input, filename):
+    import pickle
+    fw = open(filename, 'w')
+    pickle.dump(input, fw)
+    fw.close()
 
 
 data_path = "aclImdb/train/"
@@ -31,3 +51,10 @@ for neg_file in neg_files[:5]:
 
 pos_reviews = cleanText(pos_reviews)
 neg_reviews = cleanText(neg_reviews)
+
+model_google = gensim.models.Word2Vec.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+n_dim = 300
+pos_vector = [buildWordVector(model_google, z, n_dim) for z in pos_reviews]
+neg_vector = [buildWordVector(model_google, z, n_dim) for z in neg_reviews]
+storeVecs(pos_vector, 'pos_vecs.pkl')
+storeVecs(neg_vector, 'neg_vecs.pkl')
